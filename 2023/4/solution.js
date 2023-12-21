617df5e29a9075f7d7d285527473275711d89c03
Jake <jakegrimes9@gmail.com>
2023-12-21 12:05:32 -0600

    day 4 solution

--- 2023/4/solution.js
+++ 2023/4/solution.js
@@ -11,5 +11,24 @@ function get_answer_one(data){
 	}).reduce((x,y)=>x+y,0);
 }
 
+function get_answer_two(data){
+	const scores = data.map(x=>{
+		//console.log('parsing: ' + x);
+		const m = x.match(/Card\s+\d+: (.+) \| (.+)/)
+		const want = m[1].match(/\d+/g)
+		const have = m[2].match(/\d+/g)
+		const intersect = have.filter(x=>want.includes(x));
+		return intersect.length;
+	});
+	let cards = new Array(scores.length).fill(1);
+	for(let i = 0; i < cards.length; i++){
+		for (let j = i+1; j <= i+scores[i] && j < cards.length; j++){
+			cards[j] += cards[i];
+		}
+	}
+	return cards.reduce((x,y)=>x+y,0);
+}
+
 const data = readFileSync('input.txt', 'utf-8').split('\n');
 console.log(get_answer_one(data));
+console.log(get_answer_two(data));
